ebd3fac0ec64a2372feb338a0b2b11d04be7bdbe
Ildar <ildarka008@gmail.com>
2017-04-30 01:04:47 +0300

    Fix query

--- src/Api/base.js
+++ src/Api/base.js
@@ -4,7 +4,7 @@ export const request = (url, options) => {
   const optQuery = options.query || {}
   const query =
     Object.keys(optQuery)
-    .reduce((acc, key) => acc ? `${acc}&${key}=${optQuery[key]}`: `?${key}`, '')
+    .reduce((acc, key) => acc ? `${acc}&${key}=${optQuery[key]}`: `?${key}=${optQuery[key]}`, '')
   const preparedUrl = `${config.endpoint}${url}${query}`
   return fetch(preparedUrl, options).then(r => r.json())
 }
