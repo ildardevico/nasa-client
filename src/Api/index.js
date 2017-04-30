69c82528c4c720f8a86e4f1331a3c6330bb9902f
dmitry <dmitry@bizzabo.com>
2017-04-30 11:25:55 +0300

    add address

--- src/Api/index.js
+++ src/Api/index.js
@@ -6,3 +6,6 @@ export {
 } from './notify'
 
 export const getCurrCoord = () => new Promise( (ok, err) => navigator.geolocation.getCurrentPosition(ok, err));
+
+export const getAddress = (lat,lng) => fetch(`http://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&sensor=true`);
+
--- src/components/MarkerContent.js
+++ src/components/MarkerContent.js
@@ -1,31 +1,56 @@
-import React from 'react'
+import React, {Component} from 'react'
 import moment from 'moment'
 import PropTypes from 'prop-types'
 import { Button } from 'react-bootstrap'
 import {statuses} from 'constants'
+import { getAddress } from 'Api';
 
 const { number, func, string } = PropTypes
 
-const Content = ({ latitude, longitude, status, text, cb, date, _id }) => (
-  <div className="marker-item-content">
-    <span className="col-md-12">
-      <span>Latitude: {latitude}</span><br/>
-      <span>Longitude: {longitude}</span>
-    </span>
-    <span className="col-md-12">
-      Date: {moment(date).format("MM-DD-YYYY")}
-    </span>
-    {status !== 3 ? <span
-      className={`btn btn-raised btn-${statuses[status].style} offset-md-4`}
-      onClick={cb}
-    >
-      {statuses[status].button}
-    </span>: <Button bsStyle="success">{statuses[status].name}</Button>}
-  </div>
-);
+class Content extends Component {
+  state={
+    address:''
+  }
+  
+  componentDidMount() {
+    const { latitude, longitude } = this.props;
+    getAddress(latitude,longitude)
+      .then( r => r.json())
+      .then( ({results}) => {
+        if(results.length)
+          this.setState({ address: results[0].formatted_address})
+      })
+  }
+    render() {
+      const { latitude, longitude, status, text, cb, date } = this.props;
+      return (
+        <div className="marker-item-content">
+          <span className="col-md-12">
+            <span>Latitude: {latitude}</span><br/>
+            <span>Longitude: {longitude}</span>
+          </span>
+          <div role="separator" className="divider"></div>
+          <span className="col-md-12">
+            {this.state.address}
+          </span>
+          <div role="separator" className="divider"></div>
+          <span className="col-md-12">
+            Date: {moment(date).format("MM-DD-YYYY")}
+          </span>
+          <div role="separator" className="divider"></div>
+          {status !== 3 ? <span
+            className={`btn btn-raised btn-${statuses[status].style} offset-md-4`}
+            onClick={cb}
+          >
+            {statuses[status].button}
+          </span>: <Button bsStyle="success">{statuses[status].name}</Button>}
+        </div>       
+      )
+    }
+}
 
 Content.defaultProps = {
-    cb: () => {},
+    cb: () =>{},
 }
 
 Content.Proptypes = {
